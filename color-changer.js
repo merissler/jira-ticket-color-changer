8b795b10f555056d7eaee729c7b34aec38d3a814
MERissler <merissler@goodstech.com>
2025-01-14 10:01:28 -0500

    Fix

--- color-changer.js
+++ color-changer.js
@@ -1,26 +1,26 @@
-const largeNumber = 54572346582436587
+const LARGE_NUMBER = 54572346582436587
 let ticketLinkElements, ticketTextElements
 
 window.onload = function () {
-	AssignAllTicketColors()
+    AssignAllTicketColors()
 }
 
 function AssignAllTicketColors() {
-	ticketLinkElements = GetTicketLinkElements()
-	ticketTextElements = GetTicketTextElements()
-	for (let i = 0; i < ticketLinkElements.length; i++) {
-		let currentTicketLinkElement = ticketLinkElements[i]
-		let currentTicketTextElement = ticketTextElements[i]
-		if (currentTicketLinkElement !== undefined) {
-			let ticketNumber = GetTicketNumber(currentTicketLinkElement)
-			if (currentTicketLinkElement.href.includes("browse") && ticketNumber > 0) {
-				AssignTicketColor(currentTicketLinkElement, ticketNumber)
-				if (currentTicketTextElement !== undefined) {
-					AssignTicketColor(currentTicketTextElement, ticketNumber)
-				}
-			}
-		}
-	}
+    ticketLinkElements = GetTicketLinkElements()
+    ticketTextElements = GetTicketTextElements()
+    for (let i = 0; i < ticketLinkElements.length; i++) {
+        let currentTicketLinkElement = ticketLinkElements[i]
+        let currentTicketTextElement = ticketTextElements[i]
+        if (currentTicketLinkElement !== undefined) {
+            let ticketNumber = GetTicketNumber(currentTicketLinkElement)
+            if (currentTicketLinkElement.href.includes("browse") && ticketNumber > 0) {
+                AssignTicketColor(currentTicketLinkElement, ticketNumber)
+                if (currentTicketTextElement !== undefined) {
+                    AssignTicketColor(currentTicketTextElement, ticketNumber)
+                }
+            }
+        }
+    }
 }
 
 var observer = new MutationObserver(function(mutationsList, observer) {
@@ -31,39 +31,39 @@ var observer = new MutationObserver(function(mutationsList, observer) {
 observer.observe(document.body, { childList: true, subtree: true });
 
 function AssignTicketColor(ticketTextElement, seed) {
-	let uniqueHue = (seed * largeNumber) % 360
-	let uniqueColor = HsvToHex(uniqueHue, 1, 1)
-	if (ticketTextElement !== undefined) {
-		ticketTextElement.style = "color: " + uniqueColor.toString() + " !important;"
-	}
+    let uniqueHue = (seed * LARGE_NUMBER) % 360
+    let uniqueColor = HsvToHex(uniqueHue, 1, 1)
+    if (ticketTextElement !== undefined) {
+        ticketTextElement.style = "color: " + uniqueColor.toString() + " !important;"
+    }
 }
 
 function GetTicketLinkElements() {
-	// for https://goodstech.atlassian.net/jira/your-work
-	var ticketLinkElements = Array.from(document.getElementsByClassName("bu4bgh-5 dLPyHk"))
-	// for https://goodstech.atlassian.net/browse/*
-	ticketLinkElements = ticketLinkElements.concat(Array.from(document.querySelectorAll('[data-testid="issue.views.issue-base.foundation.breadcrumbs.current-issue.item"]')))
-	// for https://goodstech.atlassian.net/jira/dashboards/*
-	ticketLinkElements = ticketLinkElements.concat(Array.from(document.getElementsByClassName("issue-link")))
-	// for https://goodstech.atlassian.net/issues/*
-  	ticketLinkElements = ticketLinkElements.concat(Array.from(document.querySelectorAll('[data-testid="issue-navigator.ui.issue-results.detail-view.card-list.card"]')))
-	return ticketLinkElements
+    // for https://goodstech.atlassian.net/jira/your-work
+    var ticketLinks = Array.from(document.getElementsByClassName("bu4bgh-5 dLPyHk"))
+    // for https://goodstech.atlassian.net/browse/*
+    ticketLinks = ticketLinks.concat(Array.from(document.querySelectorAll('[data-testid="issue.views.issue-base.foundation.breadcrumbs.current-issue.item"]')))
+    // for https://goodstech.atlassian.net/jira/dashboards/*
+    ticketLinks = ticketLinks.concat(Array.from(document.getElementsByClassName("issue-link")))
+    // for https://goodstech.atlassian.net/issues/*
+    ticketLinks = ticketLinks.concat(Array.from(document.querySelectorAll('[data-testid="issue-navigator.ui.issue-results.detail-view.card-list.card"]')))
+    return ticketLinks
 }
 
 function GetTicketTextElements() {
-	// for https://goodstech.atlassian.net/jira/your-work
-	var ticketTexts = Array.from(document.getElementsByClassName("bu4bgh-0")) //bu4bgh-0 CNntJ
-	// for https://goodstech.atlassian.net/browse/*
-	ticketTexts = ticketTexts.concat(Array.from(document.getElementsByClassName("css-1buvxpt")))
-	// for https://goodstech.atlassian.net/jira/dashboards/*
-	ticketTexts = ticketTexts.concat(Array.from(document.getElementsByClassName("issue-link")))
-	// for https://goodstech.atlassian.net/issues/*
- 	ticketTexts = ticketTexts.concat(Array.from(document.getElementsByClassName("_slp31hna _1i4q1hna _1nmz1hna _otyr1b66")))
-	return ticketTexts
+    // for https://goodstech.atlassian.net/jira/your-work
+    var ticketTexts = Array.from(document.getElementsByClassName("bu4bgh-0")) //bu4bgh-0 CNntJ
+    // for https://goodstech.atlassian.net/browse/*
+    ticketTexts = ticketTexts.concat(Array.from(document.querySelectorAll('[data-testid="issue.views.issue-base.foundation.summary.heading"]')))
+    // for https://goodstech.atlassian.net/jira/dashboards/*
+    ticketTexts = ticketTexts.concat(Array.from(document.getElementsByClassName("issue-link")))
+    // for https://goodstech.atlassian.net/issues/*
+    ticketTexts = ticketTexts.concat(Array.from(document.getElementsByClassName("_slp31hna _1i4q1hna _1nmz1hna _otyr1b66")))
+    return ticketTexts
 }
 
 function GetTicketNumber(ticketElement) {
-	return ticketElement.href.match(/\d{4,}/)
+    return ticketElement.href.match(/\d{4,}/)
 }
 
 function HsvToHex(h, s, v) {
